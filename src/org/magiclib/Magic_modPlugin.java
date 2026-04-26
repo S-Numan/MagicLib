ff219c187d1bed2cb19b3d2f22d17c3038c3ebe7
S-Numan <TheSadNumanator@gmail.com>
2026-04-26 16:15:08 -0700

    Now apply paintjobs in more contexts

--- src/org/magiclib/Magic_modPlugin.java
+++ src/org/magiclib/Magic_modPlugin.java
@@ -5,6 +5,7 @@ import com.fs.starfarer.api.EveryFrameScript;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.SectorAPI;
 import com.thoughtworks.xstream.XStream;
+import org.magiclib.paintjobs.appliers.MagicPaintjobCampaignApplier;
 import org.json.JSONException;
 import org.magiclib.util.memberMemory.MemberMemoryManager;
 import org.lwjgl.util.vector.Vector2f;
@@ -128,6 +129,7 @@ public class Magic_modPlugin extends BaseModPlugin {
         SectorAPI sector = Global.getSector();
         if (sector != null) {
             sector.addTransientScript(new MagicPaintjobCampaignRefitAdder());
+            sector.addTransientScript(new MagicPaintjobCampaignApplier());
             sector.addTransientListener(new MagicIndustryItemWrangler());
             sector.addTransientScript(new MagicCampaignTrailPlugin());
         }
