1f45afca31702b7b6c4e2f70f8b046c6d985c796
S-Numan <TheSadNumanator@gmail.com>
2026-05-06 06:31:55 -0700

    New shiny icon and unused icons from Nes

--- src/org/magiclib/paintjobs/appliers/MagicPaintjobApplierUtils.kt
+++ src/org/magiclib/paintjobs/appliers/MagicPaintjobApplierUtils.kt
@@ -22,9 +22,10 @@ import org.magiclib.paintjobs.MagicPaintjobManager
 import java.awt.Color
 
 internal object MagicPaintjobApplierUtils {
-    val shinyIconColor = Color(255, 215, 0)
-    val shinyIconOpacity = 0.3f
     val shinyIconSprite = "graphics/ui/icons/32x_star_circle.png"
+    //val shinyIconSprite = "graphics/magic/icons/shiny_icon.png"
+    val shinyIconColor = Color(255, 255, 255)
+    val shinyIconOpacity = 1f
 
     fun getScreenPanel(): UIPanelAPI? {
         val state = AppDriver.getInstance().currentState
@@ -63,8 +64,11 @@ internal object MagicPaintjobApplierUtils {
             if (side != 0 && MagicPaintjobManager.getCurrentShipPaintjob(member)?.isShiny == true) {
                 val memberButton = shipList.invoke("getButtonForMember", member) as? ButtonAPI ?: return
                 if (memberButton.customData == null) { // Prevent multiple shiny icons from being applied to the same ship.
-                    val background = memberButton.parent?.addImage(shinyIconSprite, memberButton.width, memberButton.height)
-                    background?.position?.rightOfMid(memberButton, -memberButton.width)
+                    val extraSize = 8f
+                    val width = memberButton.width + extraSize
+                    val height = memberButton.height + extraSize
+                    val background = memberButton.parent?.addImage(shinyIconSprite, width, height)
+                    background?.position?.rightOfMid(memberButton, -width + extraSize / 2f)
                     background?.opacity = shinyIconOpacity
                     (background?.invoke("getSprite") as Sprite).color = shinyIconColor
                     memberButton.parent?.bringComponentToTop(memberButton)
